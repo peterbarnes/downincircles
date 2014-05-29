3348fc90f08404cf252ab091f5ff5ce79367ba40
Peter Barnes <barnespeter213@gmail.com>
2014-05-29 16:51:18 -0500

    Complete music page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,8 @@ class StaticPagesController < ApplicationController
   def contact
     
   end
+
+  def music
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/music',   to: 'static_pages#music',   via: 'get'
 end
